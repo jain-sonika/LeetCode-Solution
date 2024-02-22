19ca2109d2a06c5f3dea1f854a5da1b8c596048d
Sonika Jain <110336331+jain-sonika@users.noreply.github.com>
2024-02-22 21:31:13 +0530

    Time: 92 ms (50.25%), Space: 58.3 MB (17.50%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @param {number[][]} trust
+ * @return {number}
+ */
+var findJudge = function(n, trust) {
+    const Trusted = new Array(n+1).fill(0);
+    for(let [i,j] of trust) {
+        Trusted[i] -= 1
+        Trusted[j] += 1
+    }
+    for(let i = 1; i < Trusted.length; i++) {
+        if ((n-1) === Trusted[i]) {
+            return i;
+        }
+    }
+    return -1
+};
+// Time Complexity:- O(n) where n is the total number of people living in the town
+// Space Complexity:- O(n) where n is the total number of people living in the town
+
