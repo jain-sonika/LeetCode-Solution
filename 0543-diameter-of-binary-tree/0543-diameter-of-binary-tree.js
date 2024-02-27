450758e9671e2644e40ae7588acacb9ab82c22b6
Sonika Jain <110336331+jain-sonika@users.noreply.github.com>
2024-02-27 18:47:11 +0530

    Time: 67 ms (37.61%), Space: 51.5 MB (57.22%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function diameterOfBinaryTree(root) {
+    let max = 0
+     
+     function maxDepth(root) {
+         if (root === null) return 0 // if our root(num) is null then there is no path. return 0/null
+         let left = maxDepth(root.left) // Assign the left  of tree to LEFT; this will be easier to call it instead of writing "maxDepth(root.left)" each time
+         let right = maxDepth(root.right) //Same above
+     
+         
+         max = Math.max(max, left + right) //if the path doesn't go through the root we just get the max of them
+         return Math.max(left, right) + 1 // the path goes through the root so we add 1(for the root)
+     }
+     //since we don't know if the path will go through the root or not we will have to get the max between(path that visits the root, or the path that doesn't go through the root.)
+     maxDepth(root)
+     return max
+ };
