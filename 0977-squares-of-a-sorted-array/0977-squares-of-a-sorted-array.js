fdc5d96ae09bc40ad43ce5e4ca8fa386084cffb1
Sonika Jain <110336331+jain-sonika@users.noreply.github.com>
2024-03-02 23:00:52 +0530

    Time: 88 ms (48.41%), Space: 56.4 MB (36.94%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(A) {
+    let result = [];
+    let l = 0;
+    let r = A.length - 1;
+    let p = r;
+
+    while (l <= r) {
+        if (A[l] ** 2 > A[r] ** 2) {
+            result[p--] = A[l++] ** 2;
+        } else {
+            result[p--] = A[r--] ** 2;
+        }
+    }
+    
+    return result;
+};
