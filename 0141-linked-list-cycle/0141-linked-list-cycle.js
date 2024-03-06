551a0b79b311375463b9000f89adc9810fd44c7a
Sonika Jain <110336331+jain-sonika@users.noreply.github.com>
2024-03-06 20:28:34 +0530

    Time: 63 ms (63.59%), Space: 52.7 MB (58.65%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+    const hasCycle = (head) => {
+  let fast = head;
+  while (fast && fast.next) {
+    head = head.next;
+    fast = fast.next.next;
+    if (head === fast) return true;
+  }
+  return false;
+};
